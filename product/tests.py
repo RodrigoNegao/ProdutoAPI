b82d01c66484b821fb396299297e536ddca61881
Rodrigo da Cunha Pereira <RodrigoNegao@users.noreply.github.com>
2020-11-16 17:23:02 -0300

    updates

--- product/tests.py
+++ product/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from product.models import Product
 
-# Create your tests here.
+class ProductTestCase(TestCase):
+    def setUp(self):
+        Product.objects.create(productName="Joao", price="1")
+        print ("okss")
+        # Product.objects.create(name="cat", sound="meow")
+
+    # def test_product(self):
+    #     """test product"""
+    #     lion = Product.objects.get(name="lion")
+    #     cat = Product.objects.get(name="cat")
+    #     # self.assertEqual(lion.speak(), 'The lion says "roar"')
+    #     # self.assertEqual(cat.speak(), 'The cat says "meow"')
+
